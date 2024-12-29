8c1f369cb49d8348aa5e1c82f13058532dca45a0
ElBoyTM <nszalenski@gmail.com>
2024-12-29 17:50:40 -0500

    reverted auth.ts to original

--- Develop/server/src/services/auth.ts
+++ Develop/server/src/services/auth.ts
@@ -1,63 +1,39 @@
-import { AuthenticationError } from 'apollo-server-express';
-import { Request } from 'express';
+import type { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
-import { UserDocument } from '../models/User';
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 interface JwtPayload {
-  _id: string;
+  _id: unknown;
   username: string;
-  email: string;
+  email: string,
 }
 
-const secret = process.env.JWT_SECRET_KEY || 'mysecretsshhhhh';
-const expiration = '2h';
+export const authenticateToken = (req: Request, res: Response, next: NextFunction) => {
+  const authHeader = req.headers.authorization;
 
-export const authMiddleware = ({ req }: { req: Request }) => {
-  // Allows token to be sent via req.body, req.query, or headers
-  let token = req.body.token || req.query.token || req.headers.authorization;
+  if (authHeader) {
+    const token = authHeader.split(' ')[1];
 
-  // We split the token string into an array and return actual token
-  if (req.headers.authorization) {
-    token = token.split(' ').pop().trim();
+    const secretKey = process.env.JWT_SECRET_KEY || '';
+
+    jwt.verify(token, secretKey, (err, user) => {
+      if (err) {
+        return res.sendStatus(403); // Forbidden
+      }
+
+      req.user = user as JwtPayload;
+      return next();
+    });
+  } else {
+    res.sendStatus(401); // Unauthorized
   }
-
-  if (!token) {
-    return req;
-  }
-
-  // if token can be verified, add the decoded user's data to the request so it can be accessed in the resolver
-  try {
-    const { data } = jwt.verify(token, secret) as { data: JwtPayload };
-    req.user = data;
-  } catch {
-    console.log('Invalid token');
-  }
-
-  // return the request object so it can be passed to the resolver as `context`
-  return req;
 };
 
-export const signToken = ({ username, email, _id }: UserDocument) => {
+export const signToken = (username: string, email: string, _id: unknown) => {
   const payload = { username, email, _id };
-  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-};
+  const secretKey = process.env.JWT_SECRET_KEY || '';
 
-export const authenticateToken = (context: { req: Request }) => {
-  const authHeader = context.req.headers.authorization;
-
-  if (!authHeader) {
-    throw new AuthenticationError('You must be logged in to perform this action');
-  }
-
-  const token = authHeader.split(' ')[1];
-  if (!token) {
-    throw new AuthenticationError('Invalid token format');
-  }
-
-  try {
-    const { data } = jwt.verify(token, secret) as { data: JwtPayload };
-    return data;
-  } catch (error) {
-    throw new AuthenticationError('Invalid or expired token');
-  }
+  return jwt.sign(payload, secretKey, { expiresIn: '1h' });
 };
